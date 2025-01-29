28043dd4569d5250f3f4d52c7b58716144aeac86
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-01-29 15:56:37 -0800

    update 67

--- URET/uret/core/explorers/graph_explorer.py
+++ URET/uret/core/explorers/graph_explorer.py
@@ -154,11 +154,8 @@ class GraphExplorer(ABC):
         backcast = x[1]
         nv = x[2]
         x = x[0]
-        terminal_output = open('/dev/stdout', 'w')
-        print(x[0].shape, file=terminal_output)
-        exit(1)
 
-        original_pred, logits = self.model_predict(x[0], x[1], x[2], x[2], x[3], x[4], x[5])
+        original_pred, logits = self.model_predict(x[0], x[1], x[2], x[3], x[4], x[5], x[6])
         # print(original_pred, file=terminal_output)
         # print(original_pred.shape, file=terminal_output)
         # print(chart.shape, file=terminal_output)
