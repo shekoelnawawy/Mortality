e05ea6f0423d7000e1871fec9859e31cd5241374
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-02-04 15:24:55 -0800

    update 165

--- URET/uret/core/rankers/brute_force.py
+++ URET/uret/core/rankers/brute_force.py
@@ -86,9 +86,6 @@ class BruteForce(RankingAlgorithm):
 
                 score = scoring_function(torch.tensor(test_prob), torch.reshape(torch.tensor(test_truth), (len(torch.tensor(test_truth)),1)), torch.tensor(test_logits), True, False)
 
-                print('score', file=terminal_output)
-                print(score, file=terminal_output)
-                exit(1)
                 sample_temp = sample_temp.reshape(backcast * nv)
                 # Nawawy's end
                 if self.multi_feature_input:
